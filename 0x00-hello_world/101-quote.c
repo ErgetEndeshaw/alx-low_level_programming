7ea97dbd6687876c2e5d8a5ab409e0c205448b36
ERGET ENDESHAW <erget20@gmail.com>
2023-07-16 13:56:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - Entry point
  *
  * A c program that prints a line to the standard error
- * Return: (0) success
+ * Return: (1) success
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
 
