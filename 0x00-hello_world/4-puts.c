b250529744aa09ad9706b65dfad8a20616208d97
ERGET ENDESHAW <erget20@gmail.com>
2023-07-16 05:35:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints a line with puts function
  *
  * Rerurn: Always 0 (success)
  */
-int main (void)
+int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
