c2ffc4489d60944ae7e538165c0047660d845715
ERGET ENDESHAW <erget20@gmail.com>
2023-07-16 08:07:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * A c program that prints a line to the standard error
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
