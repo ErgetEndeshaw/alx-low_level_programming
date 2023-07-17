52d2699718cd571f904b35c780fc46cc69741f96
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 11:57:47 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Write a program that prints all possible different number
+ * Return: Always 0 (success)
+ */
+
+int main(0)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0': ones <= '9'; ones++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens =='8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
