a0b90620432d4710753571c743d2c3cf5ece1fde
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 09:11:25 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Write a program that prints all single digit numbers of base 10 starting from 0
+ * Write a program that prints all single digit numbers
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	int a;
+
 	for (a = 0; a < 10; a++)
 		printf("%d", a);
 	putchar('\n');
