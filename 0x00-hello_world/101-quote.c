1ebcc29eefd24ef73c865de834e76fe31ebf9cbe
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 14:32:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
 
