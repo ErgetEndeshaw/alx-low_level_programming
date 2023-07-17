192bd122b3f4e6ea85453168beffdc34e2f84886
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 03:44:39 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Write a program that prints the alphabet in lowercase, followed by a new line.           
+ * main - Write a program that prints the alphabet in lowercase           
  * Return: Always 0 (success)
  *
  */
