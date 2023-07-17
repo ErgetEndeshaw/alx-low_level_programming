c74ae4a7aa9a2de03fea1731e398568f95292a2f
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 12:09:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,14 @@
  * Return: Always 0 (success)
  */
 
-int main(void)
+int main(voiid)
 {
 	int ones = '0';
 	int tens = '0';
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0': ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
 			{
