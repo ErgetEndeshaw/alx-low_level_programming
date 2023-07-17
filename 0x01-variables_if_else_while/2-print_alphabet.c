3d46a3245fad6a60d5bbc88867d566c8098a6a46
ERGET ENDESHAW <erget20@gmail.com>
2023-07-17 03:05:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio .h>
+/*
+ * main -Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+		(C <= 'z') {
+			putchar(C);
+		}
+	putchar('\n');
+	return (0);
+}
