159cc91c530834bd9a232fd140130beb8ac25c1e
ERGET ENDESHAW <erget20@gmail.com>
2023-07-19 14:45:16 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,11 @@
 #include "main.h"
+/**
+ * Write a function that adds two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of the two
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
