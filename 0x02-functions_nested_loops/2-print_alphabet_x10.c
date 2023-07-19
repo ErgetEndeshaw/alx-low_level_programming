b8065ee33d144a4698ed2e0e7a2fe636e4130548
ERGET ENDESHAW <erget20@gmail.com>
2023-07-19 02:34:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; j <= 10; i++)
+	for (j = 1; j <= 10; j++)
 	{
-		for (i = a; j <= z; j++)
+		for (i = a; i <= z; i++)
 		{
-			_putchar(j);
+			_putchar(i);
 		}
 		_putchar('\n');
 	}
