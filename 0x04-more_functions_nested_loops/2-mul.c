4c041b9326b7e1a27da94ec198509348963f21de
ERGET ENDESHAW <erget20@gmail.com>
2023-07-21 13:19:24 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_mul - Multiplies two integers
+ *mul - Multiplies two integers
  * @:a- The first number to multiply
  * @:b- The second  number to be multiply with the first number
  * Return: The result of multiplication between a param and b param
  */
 
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
