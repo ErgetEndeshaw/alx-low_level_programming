38915adc8e160797833eaa8685db117663c245ac
ERGET ENDESHAW <erget20@gmail.com>
2023-07-21 12:16:52 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - Write a function that checks for a digit (0 through 9)
+ * @c: The number to be checked
+ * Return: 1 if c is a digit otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 47)
+	{
+		return (1);
+	}
+	return (0);
+}
