8b7259454807bf9873b1ab1084c2b3f8f75c9add
ERGET ENDESHAW <erget20@gmail.com>
2023-07-22 03:21:36 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point
- * main - Write a function that prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9
  * Return: 0-9 except 2 and 4
  */
+
 void print_most_numbers(void)
 {
 	int i;
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+int _putchar(char c);
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,5 +14,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+
 #endif /* MAIN_H */
