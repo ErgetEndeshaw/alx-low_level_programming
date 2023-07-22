6409d72ff6900889b98d5bc41362facc7b5ce6da
ERGET ENDESHAW <erget20@gmail.com>
2023-07-22 02:35:55 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *mul - Multiplies two integers
- * @:a- The first number to multiply
- * @:b- The second  number to be multiply with the first number
+ * mul - Multiplies two integers
+ * @a- The first number to multiply
+ * @b- The second  number to be multiply with the first number
  * Return: The result of multiplication between a param and b param
  */
 
