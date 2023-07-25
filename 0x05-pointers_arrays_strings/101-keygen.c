01064fc7c86eb185979201d8cb167cd2955f5d73
ERGET ENDESHAW <erget20@gmail.com>
2023-07-25 10:35:00 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,13 +13,14 @@ int main(void)
 
 	sum = 0;
 	srand(time(NULL));
+	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 78;
+		pass[i] = rand() % 93;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if ((3838 - sum) - '0' < 93)
 		{
-			n = 2772 - sum - '0';
+			n = 3838 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
